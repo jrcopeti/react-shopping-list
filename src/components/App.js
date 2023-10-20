c32281c82c7264a2b2b6a04d8f647a47f634c6ef
Jose Copeti <jrcopeti@gmail.com>
2023-10-20 23:40:03 +0200

    toggle item bought added

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AddItemForm from "./AddItemForm.js";
 import List from "./List.js";
 import Status from "./Status.js";
+import Title from "./Title.js";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -10,18 +11,26 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, bought: !item.bought } : item
+      )
+    );
+  }
+
   function formatCurrency(value) {
     return new Intl.NumberFormat("de-DE", {
       style: "currency",
-      currency: "EUR"
-    }).format(value)
+      currency: "EUR",
+    }).format(value);
   }
-
 
   return (
     <div>
+      <Title />
       <AddItemForm onAddItems={handleAddItems} />
-      <List items={items} onFormatCurrency={formatCurrency} />
+      <List items={items} onFormatCurrency={formatCurrency} onToggleItems={handleToggleItems} />
       <Status items={items} onFormatCurrency={formatCurrency} />
     </div>
   );
