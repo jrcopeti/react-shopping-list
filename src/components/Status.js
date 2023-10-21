0b2a0dde7e62461cb664ef7d8767c99be5e21144
Jose Copeti <jrcopeti@gmail.com>
2023-10-21 09:54:31 +0200

    display totals status

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,14 +1,22 @@
 export default function Status({ items, onFormatCurrency }) {
+  if (!items.length) return <p>No items on the list</p>;
+
+
   const totalQuantity = items.reduce((sum, item) => sum + item.quantity, 0);
   const totalPrice = items.reduce(
     (sum, item) => sum + item.price * item.quantity,
     0
   );
+  const itemsBought = items.filter((item) => item.bought).reduce((sum, item) => sum + item.quantity, 0)
+
+const percentage = (itemsBought / totalQuantity) * 100
 
   return (
-    <div>
+    <footer>
       <p>Total Items: {totalQuantity}</p>
-      <p> Total price: {totalPrice} </p>
-    </div>
+      <p>Items bought: {itemsBought}</p>
+      <p> Total price: {onFormatCurrency(totalPrice)} </p>
+      <p>{percentage === 100 ? <strong>Shopping completed</strong> : `Shopping progress: ${Math.round(percentage)}%`}</p>
+    </footer>
   );
 }
